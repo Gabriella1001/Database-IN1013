e6dc2bc3b39210bca9a1e1889f56cae7e2b98bce
Gabriella1001 <Gabriella.Jacob@city.ac.uk>
2024-10-27 11:37:13 +0000

    Updated with AUTO_INCREMENT

--- HW 4/task2.sql
+++ HW 4/task2.sql
@@ -1,9 +1,9 @@
 INSERT INTO petPet VALUES
     ("Hammy","Diane","Hampster","M","2010-10-30",NULL);
 
-INSERT INTO petEvent VALUES 
-    ("Fluffy","2020-10-15","vet","antibiotics","12"),
-    ("Hammy","2020-10-15","vet","antibiotics","13");
+INSERT INTO petEvent (petname, eventdate, eventtype, remark) VALUES 
+    ("Fluffy","2020-10-15","vet","antibiotics"),
+    ("Hammy","2020-10-15","vet","antibiotics");
 
 UPDATE petEvent 
 SET remark = "5 kittens, 3 female, 2 male"
@@ -17,7 +17,7 @@ UPDATE petPet
 SET death = "2020-09-01"
 WHERE petname = "Puffball"; 
 
-DELETE FROM petPet WHERE petname = "Buffy" AND owner = "Harold";
+DELETE FROM petPet WHERE petname = "Buffy" AND owner = "Harold"; 
 
 
 
